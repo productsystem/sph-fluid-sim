acebb3332db4eca6a75e2f946bc98c1794e8bf41
productsystem <prajitpradeep@gmail.com>
2025-06-02 02:16:23 +0530

    final commit

--- src/main.c
+++ src/main.c
@@ -2,34 +2,17 @@
 #include "particle.h"
 #include "sph.h"
 #include "rlgl.h"
-#include "rigidbody.h"
 
 int main()
 {
     InitWindow(600, 600, "SPH trial");
     SetTargetFPS(60);
     InitParticles();
-    RigidBody rb;
-    bool rbIn = false;
     while(!WindowShouldClose())
     {
-        if(IsKeyPressed(KEY_R) && !rbIn)
-        {
-            InitRigidBody(&rb, (Vector2){300,300},100,50,30);
-            rbIn = true;
-        }
-        if(rbIn)
-        {
-            ApplyBuoyancy(&rb);
-            UpdateRigidBody(&rb, DT);
-        }
         Update();
         BeginDrawing();
         ClearBackground(BLACK);
-        if(rbIn)
-        {
-            DrawRigidBody(&rb);
-        }
         DrawParticles();
         DrawFPS(10, 10);
         EndDrawing();
